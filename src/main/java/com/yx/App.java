6165624b0944d2abca659bd7b5869539e3e1aa36
yixin <39300550+1261762904@users.noreply.github.com>
2023-07-03 16:07:30 +0800

    Update App.java1

--- src/main/java/com/yx/App.java
+++ src/main/java/com/yx/App.java
@@ -26,7 +26,7 @@ public class App {
             }
         }).start();
         A();
-        System.out.println("yx");
+        System.out.println("yscjx");
     }
     private static String A(){
         String str = "";
