288863548eb72a90686d18272d29c9208f520c56
yixin <1261762904@qq.com>
2023-07-03 17:43:10 +0800

    3

--- src/main/java/com/yx/App.java
+++ src/main/java/com/yx/App.java
@@ -26,7 +26,7 @@ public class App {
             }
         }).start();
         A();
-        System.out.println("nihao2");
+        System.out.println("nihao3");
     }
     private static String A(){
         String str = "";
