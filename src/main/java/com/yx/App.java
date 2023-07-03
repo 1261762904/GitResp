a580e944536c00eac433bc8d57e7989f37003f2e
yixin <1261762904@qq.com>
2023-07-03 15:49:18 +0800

    第一次提交

---
+++ src/main/java/com/yx/App.java
@@ -0,0 +1,39 @@
+package com.yx;
+
+import com.kingdee.bos.workflow.enactment.WfEngine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+        try {
+            InputStream inputStream = new FileInputStream("./q.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(1);
+            }
+        }).start();
+        i++;
+        A();
+
+
+    }
+    private static String A(){
+        String str = "";
+        StringBuffer stringBuffer = new StringBuffer(str);
+        System.out.println("a");
+        return str;
+    }
+}
